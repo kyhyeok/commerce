a8ee75b8f43b1d9475f323e86b56df985a105dc7
김윤혁 <kyhyeok@gmail.com>
2025-07-30 16:34:11 +0900

    TDD 주기 반복
    테스트와 코드의 성장
    매개변수화 테스트
    누락된 테스트 시나리오 발견
    올바른 입력 데이터 집합
    관성에 따르는 코드 구현
    리팩터링

--- src/main/java/tdd/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/tdd/commerce/api/controller/SellerSignUpController.java
@@ -2,13 +2,39 @@ package tdd.commerce.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import tdd.commerce.command.CreateSellerCommand;
 
 @RestController
 public record SellerSignUpController() {
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+    public static final String USERNAME_REGEX = "^[a-zA-Z0-9_-]{3,}$";
 
     @PostMapping("/seller/signUp")
-    ResponseEntity<?> signUp() {
+    ResponseEntity<?> signUp(@RequestBody CreateSellerCommand command) {
+        if (!isCommandValid(command)) {
+            return ResponseEntity.badRequest().build();
+        }
+
         return ResponseEntity.noContent().build();
+    }
+
+    private static boolean isCommandValid(CreateSellerCommand command) {
+        return isEmailValid(command.email())
+            && isUsernameValid(command.username())
+            && isPasswordValid(command.password());
+    }
+
+    private static boolean isEmailValid(String email) {
+        return email != null && email.matches(EMAIL_REGEX);
+    }
+
+    private static boolean isUsernameValid(String username) {
+        return username != null && username.matches(USERNAME_REGEX);
+    }
+
+    private static boolean isPasswordValid(String password) {
+        return password != null && password.length() >= 8;
     }
 }
--- src/test/java/test/tdd/commerce/api/seller/signup/POST_space.java
+++ src/test/java/test/tdd/commerce/api/seller/signup/POST_space.java
@@ -1,8 +1,9 @@
 package test.tdd.commerce.api.seller.signup;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -10,34 +11,219 @@ import org.springframework.http.ResponseEntity;
 import tdd.commerce.CommerceApplication;
 import tdd.commerce.command.CreateSellerCommand;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
-        classes = CommerceApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+    classes = CommerceApplication.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 @DisplayName("POST /seller/signUp")
 public class POST_space {
 
     @Test
     void 올바르게_요청하면_204_No_Content_상태코드를_반환한다(
-            @Autowired TestRestTemplate client
+        @Autowired TestRestTemplate client
     ) {
         // Arrange
         var command = new CreateSellerCommand(
-                "seller@test.com",
-             "seller",
-             "password"
+            "seller@test.com",
+            "seller",
+            "password"
         );
 
         // Act
         ResponseEntity<Void> response = client.postForEntity(
             "/seller/signUp",
-                command,
-                Void.class
+            command,
+            Void.class
         );
 
         // Assert
         assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
+
+    @Test
+    void email_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            null,
+            "Seller",
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "invalid-email",
+        "invalid-email@",
+        "invalid-email@test",
+        "invalid-email@test.",
+        "invalid-email@.com.",
+    })
+    void email_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
+        String email,
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            email,
+            "Seller",
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
+
+    @Test
+    void username_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            "seller@test.com",
+            null,
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "",
+        "se",
+        "seller ",
+        "seller.",
+        "seller!",
+        "seller@",
+    })
+    void username_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
+        String username,
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            "seller@test.com",
+            username,
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "seller",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "0123456789",
+        "seller_",
+        "seller-"
+    })
+    void username_속성이_올바른_형식을_따르면_204_No_Content_상태코드를_반환한다(
+        String username,
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            "seller@test.com",
+            username,
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
+
+    @Test
+    void password_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            "seller@test.com",
+            "Seller",
+            null
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "",
+        "pass",
+        "pass123"
+    })
+    void password_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
+        String password,
+        @Autowired TestRestTemplate client
+    ) {
+// Arrange
+        var command = new CreateSellerCommand(
+            "seller@test.com",
+            "seller",
+            password
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(400);
     }
 }
